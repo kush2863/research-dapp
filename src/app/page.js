da2b648dc5df8ecd745cf3ef485797e20b7b9637
kush2863 <70647570+kush2863@users.noreply.github.com>
2024-08-28 18:51:18 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-end mr-5 mt-5'>
+      <Button> SignIn/SignUp   </Button>
+    </div>
+  )
+}
+
+export default page
