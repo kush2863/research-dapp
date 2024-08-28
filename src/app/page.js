5960d718ad45f333642fccb9fcea2bf36dcd5609
kush2863 <70647570+kush2863@users.noreply.github.com>
2024-08-28 19:09:16 +0530

    Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
+import DotPattern1 from '@/components/HomePageBackground'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
+
     <div className='flex justify-end mr-5 mt-5'>
       <Button> SignIn/SignUp   </Button>
     </div>
+    <div>
+    <DotPattern1 />
+    </div>
+    </>
   )
 }
 
---
+++ src/components/HomePageBackground.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import DotPattern from "@/components/magicui/dot-pattern";
+import { Button } from "./ui/button";
+
+const DotPattern1 = () => {
+  return (
+    <div className="relative flex h-[800px] w-full flex-col items-center justify-between overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl mt-5">
+      <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white mt-[200px]">
+        Get Mentorship and recognition for Your research from Around the globe
+      </p>
+      <Button className="w-[200px] mt-10 mb-[300px]">Get Mentorship</Button>
+      <DotPattern
+        width={20}
+        height={20}
+        cx={10}
+        cy={10}
+        cr={1}
+        className={cn(
+          "absolute inset-0 z-0", // This makes the dot pattern cover the background
+        )}
+      />
+    </div>
+  );
+};
+
+export default DotPattern1;
